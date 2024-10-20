dcd7bf3c88a67e9b7f779cc2ad5c1b94b7eca002
Mohammad Yasir <mohammadyasirkmt@gmail.com>
2024-10-20 22:10:54 +0530

    active record query 2 commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,75 +17,133 @@ class User < ApplicationRecord
 
 
 
+
+
+
+
+
+
+
+
+	# ================= some useful methods ==============
+
+	# return array
+	# User.find 1,2 we can specify multiple arg here
+	# User.first
+	# User.first(5)
+	# User.last(5)
+
+
+
+
+	# ================ these are the active record queries ===============
+
+	# return active record relation
+	# we can specify multiple condition in where
+
+	# User.where(age: 33) 
+
+	# User.where(age: 18..61).count
+	# SELECT COUNT(*) FROM `users` WHERE `users`.`age` BETWEEN 18 AND 61
+
+	# User.where(age: 18..61, id: 20..30).count
+	# SELECT COUNT(*) FROM `users` WHERE `users`.`age` BETWEEN 18 AND 61 AND `users`.`id` BETWEEN 20 AND 30
+
+	# User.where.not(age: 33).count
+	# SELECT `users`.* FROM `users` WHERE `users`.`age` != 33 
+
+	# User.where('age > ?', 19).count
+	# remeber to put ? and use '' = SELECT `users`.* FROM `users` WHERE (age > '19') to prevent SQL injection
+
+	# User.where('age > ? and gender = ?', 19, 'M')
+	# SELECT `users`.* FROM `users` WHERE (age > '19' and gender = 'M')
+
+	# User.where('name like ?', "%Jack%")
+	# SELECT `users`.* FROM `users` WHERE (name like '%Jack%')
+
+	# ordey by name, id etc ascending descending
+	# User.where('name like ?', "%Jack%").order('name DESC') for descending
+
+	# User.where('name like ?', "%Jack%").order(:id)
+	# SELECT `users`.* FROM `users` WHERE (name like '%Jack%') /* loading for pp */ ORDER BY `users`.`id`
+
+	# User.where.not('name like ?', "%Jack%").order(:id).limit(3)
+	# limiting the output
+
+	# User.where.not('name like ?', "%jack%").order(:id).limit(3).offset(2)
+	# skipping two elements
+
+	# User.where.not('name like ?', "%jack%").sum(:age)
+
+	# User.where.not('name like ?', "%jack%").pluck(:age)
+	# give array of age 
+
+	# User.where('name like ?', "%jack%").select(:name) # like id, age
+	# for getting specific column, we also use multiple params
+
+	# User.average(:id)
+
+	# User.maximum(:age)
+
+	# User.minimum(:age)
+
+	# User.where(name: "jack").explain
+	# for optimizing query
+
+	# User.where(:age => 33).update_all(:age => 29)
+	# update_all use queries like above with where method
+	# return active record relation
+
+	# user.update_attribute(:age, 19)
+	# use only on single user or user object
+
+	# user = User.find 19
+	# user.delete
+	# for deleting specific user
+
+	# User.where(:id=>20).update_all(:age => 19, :city_id => 6)
+	# updating multiple columns
+
+	# in recent version we use update and will return true/false
+
+	# user = User.new
+	# user.save 
+	# give true/false
+	# but 
+	# user.save! here ! (bang) 
+	# give error means it will show validation error
+
+
+
+	# ================== dynamic finders so below queries will be generated =================
+	# User.find_by_name("Jack Sparrow")
+
+	# User.find_by_name_and_age("Jack Sparrow", 33)
+	# SELECT `users`.* FROM `users` WHERE `users`.`name`
+	
+	# 'Jack Sparrow' AND `users`.`age` = 33
+
+
+
 	validates :first_name, presence: { message: "should be present" }, exclusion: {in: %W/admin manager/}, 
 	format: { with: /\A[A-Z]/, message: "should start with capital letter"}
 	validates :last_name, presence: true
 	validates :age, presence: true, numericality: true, inclusion: {in:(18..61)}
 	validates :city_id, presence: true
 
-	# custom validation
-	validate :check_name_uniqueness
+
+
 
 
 	# custom validation
+	# validate :check_name_uniqueness
+
 	def check_name_uniqueness
-		# ================= some useful methods ==============
-
-		# return array
-		# User.find 1,2 we can specify multiple arg here
-		# User.first
-		# User.first(5)
-		# User.last(5)
-
-
-
-
-		# ================ these are the active record queries ===============
-
-		# return active record relation
-		# we can specify multiple condition in where
-
-		# User.where(age: 33) 
-
-		# User.where(age: 18..61).count
-		# SELECT COUNT(*) FROM `users` WHERE `users`.`age` BETWEEN 18 AND 61
-
-		# User.where(age: 18..61, id: 20..30).count
-		# SELECT COUNT(*) FROM `users` WHERE `users`.`age` BETWEEN 18 AND 61 AND `users`.`id` BETWEEN 20 AND 30
-
-		# User.where.not(age: 33).count
-		# SELECT `users`.* FROM `users` WHERE `users`.`age` != 33 
-
-		# User.where('age > ?', 19).count
-		# remeber to put ? and use '' = SELECT `users`.* FROM `users` WHERE (age > '19') to prevent SQL injection
-
-		# User.where('age > ? and gender = ?', 19, 'M')
-		# SELECT `users`.* FROM `users` WHERE (age > '19' and gender = 'M')
-
-		# User.where('name like ?', "%Jack%")
-		# SELECT `users`.* FROM `users` WHERE (name like '%Jack%')
-
-		# ordey by name, id etc ascending descending
-		# User.where('name like ?', "%Jack%").order('name DESC') for descending
-
-		# User.where('name like ?', "%Jack%").order(:id)
-		# SELECT `users`.* FROM `users` WHERE (name like '%Jack%') /* loading for pp */ ORDER BY `users`.`id`
-
-		# User.where.not('name like ?', "%Jack%").order(:id).limit(3)
-		# limiting the output
-
-
-
-		# ================== dynamic finders so below queries will be generated =================
-		# User.find_by_name("Jack Sparrow")
-
-		# User.find_by_name_and_age("Jack Sparrow", 33)
-		# SELECT `users`.* FROM `users` WHERE `users`.`name`
-		
-		# 'Jack Sparrow' AND `users`.`age` = 33
 
 		exist = User.find_by_name(first_name + " " + last_name).nil? ? false : true
-		self.errors.add(:name, "already exist in db!")
+		if exist
+			self.errors.add(:name, "already exist in db!")
+		end
 		# byebug
 	end
 
