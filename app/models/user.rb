913a604bb08438fcf317e9902747370d7808cd2d
Mohammad Yasir <mohammadyasirkmt@gmail.com>
2024-10-20 19:30:24 +0530

    active record query 1:28 commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,61 @@ class User < ApplicationRecord
 
 	# custom validation
 	def check_name_uniqueness
+		# ================= some useful methods ==============
+
+		# return array
+		# User.find 1,2 we can specify multiple arg here
+		# User.first
+		# User.first(5)
+		# User.last(5)
+
+
+
+
+		# ================ these are the active record queries ===============
+
+		# return active record relation
+		# we can specify multiple condition in where
+
+		# User.where(age: 33) 
+
+		# User.where(age: 18..61).count
+		# SELECT COUNT(*) FROM `users` WHERE `users`.`age` BETWEEN 18 AND 61
+
+		# User.where(age: 18..61, id: 20..30).count
+		# SELECT COUNT(*) FROM `users` WHERE `users`.`age` BETWEEN 18 AND 61 AND `users`.`id` BETWEEN 20 AND 30
+
+		# User.where.not(age: 33).count
+		# SELECT `users`.* FROM `users` WHERE `users`.`age` != 33 
+
+		# User.where('age > ?', 19).count
+		# remeber to put ? and use '' = SELECT `users`.* FROM `users` WHERE (age > '19') to prevent SQL injection
+
+		# User.where('age > ? and gender = ?', 19, 'M')
+		# SELECT `users`.* FROM `users` WHERE (age > '19' and gender = 'M')
+
+		# User.where('name like ?', "%Jack%")
+		# SELECT `users`.* FROM `users` WHERE (name like '%Jack%')
+
+		# ordey by name, id etc ascending descending
+		# User.where('name like ?', "%Jack%").order('name DESC') for descending
+
+		# User.where('name like ?', "%Jack%").order(:id)
+		# SELECT `users`.* FROM `users` WHERE (name like '%Jack%') /* loading for pp */ ORDER BY `users`.`id`
+
+		# User.where.not('name like ?', "%Jack%").order(:id).limit(3)
+		# limiting the output
+
+
+
+		# ================== dynamic finders so below queries will be generated =================
+		# User.find_by_name("Jack Sparrow")
+
+		# User.find_by_name_and_age("Jack Sparrow", 33)
+		# SELECT `users`.* FROM `users` WHERE `users`.`name`
+		
+		# 'Jack Sparrow' AND `users`.`age` = 33
+
 		exist = User.find_by_name(first_name + " " + last_name).nil? ? false : true
 		self.errors.add(:name, "already exist in db!")
 		# byebug
